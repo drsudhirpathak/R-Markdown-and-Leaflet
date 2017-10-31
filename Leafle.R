2a68cb3714a52018c9f3e990dc0afec54a08c65d
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-31 19:42:40 +0530

    Update Leafle.R

--- Leafle.R
+++ Leafle.R
@@ -4,3 +4,5 @@ my_map<- my_map %>% addMarkers(lat=28.6129, lng= 77.2295, popup="India Gate")
 my_map<- my_map %>% addMarkers(lat=28.5450, lng= 77.1926, popup="IIT Delhi")
 my_map<- my_map %>% addMarkers(lat=28.6144, lng= 77.1996, popup="President House")
 my_map
+
+
