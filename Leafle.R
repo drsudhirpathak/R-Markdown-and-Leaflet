113814150a9022ee16851c62347a705621728664
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-11-09 19:04:58 +0530

    Update Leafle.R

--- Leafle.R
+++ Leafle.R
@@ -1,4 +1,4 @@
-# Created by Sudhir pathak On 10 October 2017
+# Created by Sudhir pathak On 7 Nnovember 2017
 my_map<- leaflet() %>% addTiles() 
 my_map<- my_map %>% addMarkers(lat=28.6129, lng= 77.2295, popup="India Gate")
 my_map<- my_map %>% addMarkers(lat=28.5450, lng= 77.1926, popup="IIT Delhi")
